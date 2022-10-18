6d5bf506453994b6911aaf52dc42ee7606cb1f4b
TanyaRampal <tanyarampal09@gmail.com>
2022-10-19 01:09:29 +0530

    added tests for add and markascomplete using jest

---
+++ __tests__/todo2.js
@@ -0,0 +1,30 @@
+// we will remove dependency of markascoplete on add
+const todoList = require("../todo");
+
+const { all, markAsComplete, add } = todoList();
+
+describe("Todolist test suit", () => {
+  beforeAll(() => {
+    add({
+      title: "test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+  });
+
+  test("should add new todo", () => {
+    const todoItemsCount = all.length;
+    add({
+      title: "test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(todoItemsCount + 1);
+  });
+
+  test("should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+});
